1f47d1574e0c110a9d18a69bf5da4af7ef530d50
Ryan Singh <ryan@mathlabs.co.uk>
2021-03-23 07:27:13 +0000

    made sure import works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Meals from "./Meals.js"
+import Meals from "./Meals.js";
+import ValidationTextFields from "./UpdateMeals.js";
 
 function App() {
   return (
     <div className="App">
       <Meals></Meals>
+      <ValidationTextFields></ValidationTextFields>
     </div>
+
   );
 }
 
